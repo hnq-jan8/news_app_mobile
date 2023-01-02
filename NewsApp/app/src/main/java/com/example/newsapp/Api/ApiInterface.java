794de9ceb45d79d787d52e68a7c4d6c114e74d16
Lê Anh Vũ <vula.bi11-288@st.usth.edu.vn>
2023-01-02 22:32:58 +0700

    Update

---
+++ NewsApp/app/src/main/java/com/example/newsapp/Api/ApiInterface.java
@@ -0,0 +1,25 @@
+package com.example.newsapp.Api;
+
+import com.example.newsapp.Models.ApiResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+    @GET("top-headlines")
+    Call<ApiResponse> callHeadlines(
+            @Query("country") String country,
+            @Query("category") String category,
+            @Query("q") String query,
+            @Query("apiKey") String api_key
+    );
+
+    @GET("everything")
+    Call<ApiResponse> callSpecificHeadlines(
+            @Query("q") String query,
+            @Query("apiKey") String api_key
+    );
+
+
+}
---
+++ NewsApp/app/src/main/res/drawable-v24/Api/ApiRequest.java
@@ -0,0 +1,52 @@
+package com.example.newsapp.Api;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.example.newsapp.Api.ApiInterface;
+import com.example.newsapp.FetchData;
+import com.example.newsapp.Models.ApiResponse;
+import com.example.newsapp.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiRequest {
+    Context context;
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+    public void getNewsHeadlines(FetchData listener, String category, String query)
+    {
+        ApiInterface callNewsApi = retrofit.create(ApiInterface.class);
+        Call<ApiResponse> call = callNewsApi.callHeadlines("us",category,query,context.getString(R.string.api_key));
+
+        try{
+            call.enqueue(new Callback<ApiResponse>() {
+                @Override
+                public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
+                    listener.onFetchData(response.body().getArticles(), response.message());
+
+                }
+
+                @Override
+                public void onFailure(Call<ApiResponse> call, Throwable t) {
+                    listener.onError("Request Failed");
+                }
+            });
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    public ApiRequest(Context context) { this.context = context;}
+
+}
